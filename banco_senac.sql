32841538e79b995b9da9301926120845848f9f3b
Susan Karin Soares Marques <100443975+SusanMarques@users.noreply.github.com>
2024-03-25 17:24:50 -0300

    Update banco_senac.sql
    atualização do script do banco senac feito na aula do dia 25/03

--- banco_senac.sql
+++ banco_senac.sql
@@ -72,6 +72,8 @@ CREATE TABLE uc(
 CREATE TABLE professor(
 	cpf char(11) foreign key references funcionario(cpf),
 );
+
+
 CREATE TABLE pedagogo(
 	cpf char(11) foreign key references funcionario(cpf),
 );
@@ -142,3 +144,74 @@ insert into Senac(id_unidade,nome_da_unidade,cnpj,url_site_senac,email,endereco,
 VALUES
 (1,'SENAC ZONA NORTE','12345678912345','senaczn.com.br','senaczn@gmail.com','Tv. Macaé, 2875 - Potengi, Natal','845231649'),
 (2,'SENAC CENTRO','78945612352698','senaccentro.com.br','senaccentro@gmail.com',' R. São Tomé, 444 - Cidade Alta','84963251')
+
+select * from Senac;
+
+insert into curso(id_do_curso,data_de_inicio,data_de_termino,professores,coordenador,nome_do_curso)
+VALUES
+(100,'10-01-2024','10-12-2024','valtemir,','maria','Operador de computadores'),
+(101,'01-01-2024','10-12-2025','pedro,','maria','design'),
+(102,'11-05-2024','10-12-2024','joao,','maria','corel draw'),
+(103,'10-12-2024','10-12-2025','ana,','marcos','photoshop')
+
+select * from curso;
+
+
+UPDATE curso SET categoria = 'TI' WHERE id_do_curso = 100;
+UPDATE curso SET categoria = 'DESIGN' WHERE id_do_curso = 101;
+UPDATE curso SET categoria = 'DESIGN' WHERE id_do_curso = 102;
+UPDATE curso SET categoria = 'DESIGN' WHERE id_do_curso = 103;
+
+UPDATE curso SET carga_horaria = 20 WHERE id_do_curso = 100;
+UPDATE curso SET carga_horaria = 40 WHERE id_do_curso = 101;
+UPDATE curso SET carga_horaria = 30 WHERE id_do_curso = 102;
+UPDATE curso SET carga_horaria = 20 WHERE id_do_curso = 103;
+
+UPDATE curso SET preco = 130.00 WHERE id_do_curso = 100;
+UPDATE curso SET preco = 200 WHERE id_do_curso = 101;
+UPDATE curso SET preco = 350 WHERE id_do_curso = 102;
+UPDATE curso SET preco = 100 WHERE id_do_curso = 103;
+
+UPDATE curso SET qnt_vagas = 100 WHERE id_do_curso = 100;
+UPDATE curso SET qnt_vagas = 50.99 WHERE id_do_curso = 101;
+UPDATE curso SET qnt_vagas = 25 WHERE id_do_curso = 102;
+UPDATE curso SET qnt_vagas = 10.99 WHERE id_do_curso = 103;
+
+insert into funcionario(nome,telefone,endereco,cpf,cargo,email,salario,data_de_admissao)
+VALUES 
+('valtemir','863550768','Vale Quem Tem,Teresina','70651489635','professor','valtemir@spires.com.br',10.000,'01-01-2023'),
+('maria','465982134','Rua Dona Maria Amélia','45678912345','coordenador','maria@gmail.com.br',20.000,'01-01-2022'),
+('marcos','852963741','Travessa Principal','78963214789','coordenador','marcos@spires.com.br',30.000,'01-12-2023'),
+('pedro','859674123','Rua Guaiana','85963214569','professor','pedro@spires.com.br',10.000,'01-01-2020'),
+('joao','986532148','Rua Estados Unidos','12345678521','professor','joao@spires.com.br',10.000,'01-01-2021'),
+('ana','896532147','Rua Conceição Alvarenga','78945612354','professor','ana@spires.com.br',5.000,'01-01-2024')
+
+select * from funcionario;
+
+insert into aluno(matricula,nome,endereco,telefone,cpf,email,genero,data_de_nascimento)
+VALUES
+('1010','Luna Brenda Stefany Cardoso','Quadra I1','849653251','72904315888','luna_cardoso@zepto.com.br','F','13-02-1958'),
+('1011','Valentina Letícia Raquel Moraes','Avenida Vinte e Cinco de Julho','123456632','65336124024','valentinaleticiamoraes@temp.com.br','F','10-01-1987'),
+('1012','Thiago André Teixeira','Rua Seis de Maio','156325698','57178867485','thiago.andre.teixeira@infolink.com.br','M','06-01-1978'),
+('1013','Lorenzo Antonio Emanuel Gonçalves','Rua Newton Braga','968325741','94125434840','94125434840','M','13-01-1947')
+
+
+select * from aluno;
+
+insert into matricula(id_do_curso,matricula)
+VALUES
+(100,'1010'),
+(101,'1011'),
+(102,'1012'),
+(103,'1013')
+
+select * from matricula;
+
+insert into professor(cpf)
+VALUES
+('70651489635'),
+('85963214569'),
+('12345678521'),
+('78945612354')
+
+select * from professor;
